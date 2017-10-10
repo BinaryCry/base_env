3467a479c6978803d53b0b1886f98623c62bc9d7
Alex Step <stepalexandr@gmail.com>
2017-10-10 15:55:39 +0300

    conditional rendering

--- web/js/bundle.js
+++ web/js/bundle.js
@@ -20964,21 +20964,37 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function Geet(props) {
-
-    console.log(props);
-
-    if (props.params.logstatus) {
-        return _react2.default.createElement(
-            'div',
-            { onClick: props.onClick },
-            'Hello, ',
-            props.params.data.name
-        );
-    }
+    if (props.logstatus) return _react2.default.createElement(
+        'div',
+        null,
+        'Hello, user!'
+    );
     return _react2.default.createElement(
         'div',
+        null,
+        'Please Sign Up or Sign In'
+    );
+}
+function LoginButton(props) {
+    return _react2.default.createElement(
+        'button',
         { onClick: props.onClick },
-        'Please SignIn or SignUp!'
+        'Please Log In'
+    );
+}
+function LogOutButton(props) {
+    return _react2.default.createElement(
+        'button',
+        { onClick: props.onClick },
+        'Log Out'
+    );
+}
+function RegLink(props) {
+    if (props.logstatus) return null;
+    return _react2.default.createElement(
+        'a',
+        { href: '/registration' },
+        'registration'
     );
 }
 
@@ -20990,30 +21006,51 @@ var SignForm = function (_Component) {
 
         var _this = _possibleConstructorReturn(this, (SignForm.__proto__ || Object.getPrototypeOf(SignForm)).call(this, props));
 
-        _this.state = {}; // prevState setState()
+        _this.state = {
+            logstatus: false
+        };
+        _this.login = _this.login.bind(_this);
+        _this.logout = _this.logout.bind(_this);
         return _this;
     }
 
     _createClass(SignForm, [{
         key: 'render',
         value: function render() {
+            var logstatus = this.state.logstatus;
+            var button = null;
+
+            if (logstatus) button = _react2.default.createElement(LogOutButton, { onClick: this.logout });else button = _react2.default.createElement(LoginButton, { onClick: this.login });
+
             return _react2.default.createElement(
-                'form',
-                { action: '/' },
-                _react2.default.createElement(Geet, { params: this.props, onClick: this.handleClick })
+                'div',
+                null,
+                _react2.default.createElement(Geet, { logstatus: this.state.logstatus }),
+                button,
+                _react2.default.createElement('br', null),
+                _react2.default.createElement(RegLink, { logstatus: this.state.logstatus })
             );
         }
     }, {
-        key: 'handleClick',
-        value: function handleClick() {
-            alert(1);
+        key: 'login',
+        value: function login() {
+            this.setState(function (prevState) {
+                return { logstatus: !prevState.logstatus };
+            });
+        }
+    }, {
+        key: 'logout',
+        value: function logout() {
+            this.setState(function (prevState) {
+                return { logstatus: !prevState.logstatus };
+            });
         }
     }]);
 
     return SignForm;
 }(_react.Component);
 
-(0, _reactDom.render)(_react2.default.createElement(SignForm, { logstatus: false, data: { name: 'John' } }), document.getElementById('root'));
+(0, _reactDom.render)(_react2.default.createElement(SignForm, null), document.getElementById('root'));
 
 
 },{"react":31,"react-dom":28}]},{},[32]);
--- web/js/index.jsx
+++ web/js/index.jsx
@@ -1,34 +1,53 @@
 import React, { Component }  from 'react';
 import { render } from 'react-dom'
 
-
-
-function HelloUser() {
-    return <div>Hello, User</div>
-}
-function SignUpIn() {
+function Geet(props) {
+    if (props.logstatus) return <div>Hello, user!</div>;
     return <div>Please Sign Up or Sign In</div>
 }
-
-
+function LoginButton(props) {
+    return <button onClick={props.onClick}>Please Log In</button>
+}
+function LogOutButton(props) {
+    return <button onClick={props.onClick}>Log Out</button>
+}
+function RegLink(props) {
+    if( props.logstatus ) return null;
+    return <a href="/registration">registration</a>
+}
 
 class SignForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
-        }; // prevState setState()
+            logstatus: false
+        };
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
     }
     render() {
+        let logstatus = this.state.logstatus;
+        let button = null;
+
+        if (logstatus)
+            button = <LogOutButton onClick={ this.logout }/>;
+        else button = <LoginButton onClick={ this.login }/>;
+
         return (
-            <form action="/">
-                <Geet params={this.props} onClick={this.handleClick} />
-            </form>
-        )
+            <div>
+                <Geet logstatus={ this.state.logstatus } />
+                { button }
+                <br/>
+                <RegLink logstatus={ this.state.logstatus } />
+            </div>
+        );
     }
-    handleClick() {
-        alert(1)
+    login() {
+        this.setState( prevState => ({ logstatus: !prevState.logstatus }) );
+    }
+    logout() {
+        this.setState( function(prevState) { return { logstatus: !prevState.logstatus } }  );
     }
 }
 
-render( <SignForm logstatus={ false } data={ { name: 'John' } } />, document.getElementById('root') );
+render( <SignForm />, document.getElementById('root') );
