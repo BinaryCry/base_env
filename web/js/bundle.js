07b8a7e5560c6c63cf548c5bfdeee092a216a2cd
Alex Step <stepalexandr@gmail.com>
2017-10-12 17:58:36 +0300

    PropTypes

--- web/js/bundle.js
+++ web/js/bundle.js
@@ -20955,6 +20955,10 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactDom = require('react-dom');
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -20963,135 +20967,35 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var TempInput = function (_Component) {
-    _inherits(TempInput, _Component);
+var Geet = function (_Component) {
+    _inherits(Geet, _Component);
 
-    function TempInput(props) {
-        _classCallCheck(this, TempInput);
+    function Geet(props) {
+        _classCallCheck(this, Geet);
 
-        return _possibleConstructorReturn(this, (TempInput.__proto__ || Object.getPrototypeOf(TempInput)).call(this, props));
+        return _possibleConstructorReturn(this, (Geet.__proto__ || Object.getPrototypeOf(Geet)).call(this, props));
     }
 
-    _createClass(TempInput, [{
+    _createClass(Geet, [{
         key: 'render',
         value: function render() {
             return _react2.default.createElement(
                 'p',
                 null,
-                'Input in ',
-                this.props.scale,
-                ': ',
-                _react2.default.createElement('input', { onChange: this.props.onTeempChange, type: 'text', value: this.props.currentTemp })
+                'Hello, ',
+                this.props.user
             );
         }
     }]);
 
-    return TempInput;
+    return Geet;
 }(_react.Component);
 
-var BoilVerdict = function (_Component2) {
-    _inherits(BoilVerdict, _Component2);
+Geet.propTypes = {
+    user: _propTypes2.default.string
+};
 
-    function BoilVerdict(props) {
-        _classCallCheck(this, BoilVerdict);
-
-        return _possibleConstructorReturn(this, (BoilVerdict.__proto__ || Object.getPrototypeOf(BoilVerdict)).call(this, props));
-    }
-
-    _createClass(BoilVerdict, [{
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'p',
-                null,
-                _react2.default.createElement(
-                    'b',
-                    null,
-                    this.props.substance,
-                    ' is ',
-                    this.props.temp >= this.props.boilTemp ? '' : 'not',
-                    ' boiled'
-                )
-            );
-        }
-    }]);
-
-    return BoilVerdict;
-}(_react.Component);
-
-var Boil = function (_Component3) {
-    _inherits(Boil, _Component3);
-
-    function Boil(props) {
-        _classCallCheck(this, Boil);
-
-        var _this3 = _possibleConstructorReturn(this, (Boil.__proto__ || Object.getPrototypeOf(Boil)).call(this, props));
-
-        _this3.fahrenheitState = _this3.fahrenheitState.bind(_this3);
-        _this3.celsiumState = _this3.celsiumState.bind(_this3);
-        _this3.boilTemps = {
-            water: {
-                c: 100,
-                f: 212
-            }
-        };
-        _this3.scales = {
-            f: 'Fahrenheit',
-            c: 'Celsium'
-        };
-        _this3.initialState = {
-            f_degr: '',
-            c_degr: ''
-        };
-        _this3.state = _this3.initialState;
-        return _this3;
-    }
-
-    _createClass(Boil, [{
-        key: 'fahrenheitState',
-        value: function fahrenheitState(syntEvent) {
-            var value = syntEvent.target.value;
-            if (value == '') {
-                this.setState(this.initialState);
-                return false;
-            }
-            var c_degr = Math.round((value - 32) * 5 / 9 * 10) / 10;
-            this.setState({
-                f_degr: value,
-                c_degr: !c_degr ? 'incorrect' : c_degr
-            });
-        }
-    }, {
-        key: 'celsiumState',
-        value: function celsiumState(syntEvent) {
-            var value = syntEvent.target.value;
-            if (value == '') {
-                this.setState(this.initialState);
-                return false;
-            }
-            var f_degr = Math.round((value * 9 / 5 + 32) * 10) / 10;
-            this.setState({
-                c_degr: value,
-                f_degr: !f_degr ? 'incorrect' : f_degr
-            });
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'div',
-                null,
-                _react2.default.createElement(TempInput, { scale: this.scales.c, onTeempChange: this.celsiumState, currentTemp: this.state.c_degr }),
-                _react2.default.createElement(TempInput, { scale: this.scales.f, onTeempChange: this.fahrenheitState, currentTemp: this.state.f_degr }),
-                _react2.default.createElement(BoilVerdict, { temp: this.state.c_degr, substance: this.props.substance, boilTemp: this.boilTemps[this.props.substance].c })
-            );
-        }
-    }]);
-
-    return Boil;
-}(_react.Component);
-
-(0, _reactDom.render)(_react2.default.createElement(Boil, { substance: 'water' }), document.getElementById('root'));
+(0, _reactDom.render)(_react2.default.createElement(Geet, { user: 1 }), document.getElementById('root'));
 
 
-},{"react":31,"react-dom":28}]},{},[32]);
+},{"prop-types":24,"react":31,"react-dom":28}]},{},[32]);
--- web/js/index.jsx
+++ web/js/index.jsx
@@ -1,84 +1,17 @@
 import React, { Component }  from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
-class TempInput extends Component {
+class Geet extends Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return <p>Input in {this.props.scale}: <input onChange={this.props.onTeempChange} type="text" value={this.props.currentTemp}/></p>
+        return <p>Hello, {this.props.user}</p>
     }
 }
-class BoilVerdict extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return(
-            <p>
-                <b>{this.props.substance} is { this.props.temp >= this.props.boilTemp ? '' : 'not' } boiled</b>
-            </p>
-        )
-    }
-}
-class Boil extends Component {
-    constructor(props) {
-        super(props);
-        this.fahrenheitState = this.fahrenheitState.bind(this);
-        this.celsiumState = this.celsiumState.bind(this);
-        this.boilTemps = {
-            water: {
-                c: 100,
-                f: 212
-            }
-        };
-        this.scales = {
-            f: 'Fahrenheit',
-            c: 'Celsium'
-        };
-        this.initialState = {
-            f_degr: '',
-            c_degr: ''
-        };
-        this.state = this.initialState;
-    }
-    fahrenheitState(syntEvent) {
-        let value = syntEvent.target.value;
-        if( value == '' ) {
-            this.setState(this.initialState);
-            return false;
-        }
-        let  c_degr = Math.round(((value - 32) * 5 / 9) * 10) / 10;
-        this.setState(
-            {
-                f_degr: value,
-                c_degr: !c_degr ? 'incorrect' : c_degr
-            }
-        );
-    }
-    celsiumState(syntEvent) {
-        let value = syntEvent.target.value;
-        if( value == '' ) {
-            this.setState(this.initialState);
-            return false;
-        }
-        let f_degr = Math.round(((value * 9 / 5) + 32) * 10) / 10;
-        this.setState(
-            {
-                c_degr: value,
-                f_degr: !f_degr ? 'incorrect' : f_degr
-            }
-        );
-    }
-    render() {
-        return(
-            <div>
-                <TempInput scale={this.scales.c} onTeempChange={this.celsiumState} currentTemp={this.state.c_degr} />
-                <TempInput scale={this.scales.f} onTeempChange={this.fahrenheitState} currentTemp={this.state.f_degr} />
-                <BoilVerdict temp={this.state.c_degr} substance={this.props.substance} boilTemp={this.boilTemps[this.props.substance].c} />
-            </div>
-        )
-    }
-}
+Geet.propTypes = {
+    user: PropTypes.string
+};
 
-render( <Boil substance="water" />, document.getElementById('root') );
+render(<Geet user={1} />, document.getElementById('root'));
